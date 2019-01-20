b57301bfb103ab82b72d652df885d6f8749b5569
gokayumutlu <gokay.umutlu.353@outlook.com>
2019-01-20 03:37:00 +0300

    latest changes 2001

--- routes/router/ilacTakipGetir.js
+++ routes/router/ilacTakipGetir.js
@@ -2,19 +2,23 @@ const express = require("express");
 const mysql=require("./../../db/db").pool;
 const router = express.Router();
 const bodyParser=require("body-parser");
+const underscore=require("underscore");
 
 router.get("/",(req,res)=>{
     mysql.getConnection(function(err,conn){
-        var email=req.body.email;
+        var email=req.query.email;
         var sql="select ilac_adi, adet, aciklama from ((((ilactakip inner join ogrenci on ilactakip.ogrenci_id=ogrenci.ogrenci_id) inner join sinif on ogrenci.sinif_id=sinif.sinif_id) inner join ogretmen on sinif.ogretmen_id=ogretmen.ogretmen_id) inner join kullanici on ogretmen.kullanici_id=kullanici.kullanici_id) where kullanici.email=(?)";
         conn.query(sql,email,function(err,rows){
+            console.log(email);
             if(err){
                 res.status(404).send({error: err})
                 //res.json(err);
             }
+            
             else{
-                res.json(rows);
-                console.log(rows);
+                
+                res.send(JSON.parse(JSON.stringify({ilacData: rows})));
+                console.log(JSON.parse(JSON.stringify(rows)));
             }
         })
         conn.release();
